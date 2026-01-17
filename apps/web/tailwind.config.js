f662e39715df6ee537f5bc0fca2c1ab0cfbb9419
bhavenmurji <bhavenmurji@gmail.com>
2026-01-17 14:12:35 -0500

    🔖 Checkpoint: Edit /Users/bhavenmurji/Development/IgniteHealthSystems/apps/dpc-comparator/apps/web/tailwind.config.js

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
